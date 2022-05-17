ad72af0753ded29101e2595fba4a01c4ccf16cf8
“rotureau.martin@gmail.com” <rotureau.martin@gmail.com>
2022-05-17 11:47:42 +0200

    setup controllers and repositories with CRUD methods

---
+++ server/src/controllers/productController.ts
@@ -0,0 +1,65 @@
+import express, { NextFunction } from "express";
+import { Product } from "../entities/Product";
+import { createProductIfNotExists, deleteManyProducts, getManyProducts, updateProduct } from "../repositories/productRepository";
+
+interface TypedRequestBody<T> extends express.Request {
+    body: T;
+}
+
+type CreateProductInput = {
+    name: string;
+    price: number;
+};
+
+type UpdateProductInput = {
+    uuid: string;
+    name: string;
+    price: number;
+};
+
+type DeleteProductInput = {
+    productUuids: string[];
+};
+
+export const createProductController = async (req: TypedRequestBody<CreateProductInput>, res: express.Response, next: NextFunction) => {
+    try {
+        const product = await createProductIfNotExists({ ...req.body });
+        return res.status(200).send(product);
+    } catch (e) {
+        next(e);
+    }
+};
+
+export const getManyProductController = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    try {
+        const products = await getManyProducts(req.query.productUuids as string[]);
+        res.status(200).send(products);
+    } catch (e) {
+        next(e);
+    }
+};
+
+export const updateProductController = async (req: TypedRequestBody<UpdateProductInput>, res: express.Response, next: NextFunction) => {
+    try {
+        const product = await updateProduct({
+            where: {
+                where: {
+                    uuid: req.body.uuid,
+                },
+            },
+            set: { ...(req.body as Product) },
+        });
+        return res.status(200).send(product);
+    } catch (e) {
+        next(e);
+    }
+};
+
+export const deleteProductController = async (req: TypedRequestBody<DeleteProductInput>, res: express.Response, next: NextFunction) => {
+    try {
+        await deleteManyProducts(req.body.productUuids);
+        return res.status(204);
+    } catch (e) {
+        next(e);
+    }
+};
---
+++ server/src/repositories/productRepository.ts
@@ -0,0 +1,28 @@
+import { DeepPartial, FindOneOptions, In } from "typeorm";
+import dataSource from "../config/typeormConfig";
+import { Product } from "../entities/Product";
+
+export const createProductIfNotExists = async (params: DeepPartial<Product>): Promise<Product | null> => {
+    const repository = dataSource.getRepository(Product);
+    const product = await repository.findOne({ where: { name: params.name } });
+    if (!product) {
+        return await repository.save(params);
+    }
+    return null;
+};
+
+export const updateProduct = async ({ where, set }: { where: FindOneOptions<Product>; set: Product }): Promise<Product> => {
+    const repository = dataSource.getRepository(Product);
+    const product = await repository.findOneOrFail(where);
+    return await repository.save({ ...product, ...set });
+};
+
+export const getManyProducts = async (productUuids: string[]) => {
+    const repository = dataSource.getRepository(Product);
+    return await repository.find({ where: { uuid: In(productUuids) } });
+};
+
+export const deleteManyProducts = async (productUuids: string[]) => {
+    const repository = dataSource.getRepository(Product);
+    return await repository.delete(productUuids);
+};
